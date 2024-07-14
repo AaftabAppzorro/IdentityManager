26a360f31b8b800d6693e44c40b57880c7da4a9e
Aaftab <161571110+AaftabAppzorro@users.noreply.github.com>
2024-07-14 19:51:47 +0530

    Create ApplicationDbContext.cs

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using IdentityManager.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityManager.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+    }
+}
